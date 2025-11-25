facaa4e77298486588e5b22a84cbdc6b6b49855f
Evgalion2021 <zhenya277277@gmail.com>
2025-11-25 11:26:31 +0200

    fix(): next-intl fix root page

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,5 +4,17 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  // Match only internationalized pathnames
+  matcher: [
+    // Enable a redirect to a matching locale at the root
+    "/",
+    // Set a cookie to remember the previous locale for
+    // all requests that have a locale prefix
+    // `/(uk|en)/:path*`,
+    `/(uk)/:path*`,
+
+    // Skip all paths that should not be internationalized
+    "/((?!_next|_vercel|api|robots.txt|favicon.ico|sitemap|.*\\..*).*)",
+  ],
+  // matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
