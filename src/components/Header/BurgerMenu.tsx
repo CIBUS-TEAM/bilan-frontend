8151d85c5825e86b5fa2594300b070b6a0acf0df
Evgalion2021 <zhenya277277@gmail.com>
2025-12-04 17:26:04 +0200

    fix(): resolve conversations

--- src/components/Header/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { cn } from "@/utilities/styles";
 import { useState } from "react";
 import NavLink from "../NavLink/NavLink";
 import StrapiButton from "../Button/Button";
@@ -21,29 +21,33 @@ export default function BurgerMenu({ links, button }: BurgerMenuProps) {
         className="cursor-pointer w-8 h-6 flex flex-col justify-between items-center"
       >
         <span
-          className={`h-1 w-full bg-primary rounded transition-all duration-300 ${
-            isOpen ? "translate-y-2.5 rotate-45" : ""
-          }`}
+          className={cn(
+            "h-1 w-full bg-primary rounded transition-all duration-300",
+            { "translate-y-2.5 rotate-45": isOpen }
+          )}
         ></span>
         <span
-          className={`h-1 w-full bg-primary rounded transition-all duration-300 ${
-            isOpen ? "opacity-0" : ""
-          }`}
+          className={cn(
+            "h-1 w-full bg-primary rounded transition-all duration-300",
+            { "opacity-0": isOpen }
+          )}
         ></span>
         <span
-          className={`h-1 w-full bg-primary rounded transition-all duration-300 ${
-            isOpen ? "-translate-y-2.5 -rotate-45" : ""
-          }`}
+          className={cn(
+            "h-1 w-full bg-primary rounded transition-all duration-300",
+            { "-translate-y-2.5 -rotate-45": isOpen }
+          )}
         ></span>
       </button>
 
       <nav
-        className={`
-          fixed top-14.5 lg:top-18.5 right-0 h-screen w-full bg-white
-          transform transition-transform duration-300
-          pt-10 px-4 pb-28 flex flex-col gap-6 border-t border-border
-          ${isOpen ? "translate-x-0" : "translate-x-full"}
-        `}
+        className={cn(
+          "fixed top-14.5 lg:top-18.5 right-0 h-screen w-full bg-white",
+          "transform transition-transform duration-300",
+          "pt-10 px-4 pb-28 flex flex-col gap-6 border-t border-border",
+          { "translate-x-0": isOpen },
+          { "translate-x-full": !isOpen }
+        )}
       >
         {links.map((link) => (
           <NavLink
